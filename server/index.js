2a3347aff670e081ac2d0d67ac999901523507bb
Nick Dubelman <ndubelman@arista.com>
2018-03-17 01:02:09 -0700

    added typescript as backend dependency

--- server/index.js
+++ server/index.js
@@ -1,18 +1,15 @@
-import stream from 'stream'
-import express from 'express'
-const app = express()
-
-app.get('/', ( req, res ) => {
-    var fileContents = Buffer.from('example css file')
-
-    var readStream = new stream.PassThrough()
-    readStream.end(fileContents)
-  
-    res.set('Content-Disposition', 'attachment; filename=theme.css')
-    res.set('Content-Type', 'text/plain')
-  
-    readStream.pipe(res)
-})
-
-const port = 8080
-app.listen( port, () => console.log(`Server running on port ${port}...`) )
+"use strict";
+exports.__esModule = true;
+var stream = require("stream");
+var express = require("express");
+var app = express();
+app.get('/', function (req, res) {
+    var fileContents = Buffer.from('example css file');
+    var readStream = new stream.PassThrough();
+    readStream.end(fileContents);
+    res.set('Content-Disposition', 'attachment; filename=theme.css');
+    res.set('Content-Type', 'text/plain');
+    readStream.pipe(res);
+});
+var port = 8080;
+app.listen(port, function () { return console.log("Server running on port " + port + "..."); });
